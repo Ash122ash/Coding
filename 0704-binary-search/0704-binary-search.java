3210d8db04507d52f53219d70479f28285069497
Aayush Sharma <97503888+Ash122ash@users.noreply.github.com>
2026-07-27 22:53:37 +0530

    Time: 0 ms (100%), Space: 48.1 MB (92.16%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0){
+            return -1;
+        }
+        int n=nums.length;
+        int s=0;
+        int e=n-1;
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]<target){
+                s=mid+1;
+            }
+            else{
+                e=mid-1;
+            }
+        }
+        return -1;
+
+    }
+}
