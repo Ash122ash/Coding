99de86d0745bbd5718c2119cb14f33472459b4ce
Aayush Sharma <97503888+Ash122ash@users.noreply.github.com>
2026-08-18 12:05:28 +0530

    Time: 0 ms (100%), Space: 43.6 MB (70.86%) - LeetHub

--- 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.java
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.java
@@ -15,46 +15,22 @@
  */
 
 class Solution {
-
-    public int sum(TreeNode root, StringBuilder a) {
-
-        if (root == null) {
-            return 0;
-        }
-
-        // Add current node to binary string
-        a.append(root.val);
-
-        // If leaf node, convert binary string to decimal
-        if (root.left == null && root.right == null) {
-            int num = Integer.parseInt(a.toString(), 2);
-
-            // Remove current node before returning
-            a.deleteCharAt(a.length() - 1);
-
-            return num;
-        }
-
-        // Go left
-        int left = sum(root.left, a);
-
-        // Go right
-        int right = sum(root.right, a);
-
-        // Remove current node while going back
-        a.deleteCharAt(a.length() - 1);
-
-        return left + right;
-    }
-
+    
     public int sumRootToLeaf(TreeNode root) {
-
-        if (root == null) {
+        return dfs(root, 0);
+    }
+    
+    private int dfs(TreeNode node, int current) {
+        if (node == null) {
             return 0;
         }
-
-        StringBuilder a = new StringBuilder();
-
-        return sum(root, a);
+        
+        current = current * 2 + node.val;
+        
+        if (node.left == null && node.right == null) {
+            return current;
+        }
+        
+        return dfs(node.left, current) + dfs(node.right, current);
     }
 }
